59f4d09b876151a71dcdf0a678f2d2ab0a3f05f4
AswathiMohan23 <achu999here@gmail.com>
2022-11-05 19:50:23 +0530

    [ADDED] : a method appendingElements in LinkedList.java to append the elements

--- src/com/java/LinkedListCode.java
+++ src/com/java/LinkedListCode.java
@@ -3,10 +3,20 @@ package com.java;
 import java.util.LinkedList;
 
 public class LinkedListCode {
+    static LinkedListMethods linkedListMethods=new LinkedListMethods();
 
     public static void main(String[] args){
         createSimpleLinkedList();
+        appendingTheElements();
     }
+
+    public static void appendingTheElements() {
+        linkedListMethods.append(56);
+        linkedListMethods.append(70);
+        linkedListMethods.append(30);
+        linkedListMethods.display();
+    }
+
     public static void createSimpleLinkedList() {
         LinkedList<Integer> list = new LinkedList<>();
         list.add(56);
