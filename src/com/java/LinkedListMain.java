ae870af3b3ad4044fe64771e057c6cdfa5e41671
AswathiMohan23 <achu999here@gmail.com>
2022-11-05 23:25:02 +0530

    [ADDED] : code to delete the first element

--- src/com/java/LinkedListMain.java
+++ src/com/java/LinkedListMain.java
@@ -10,25 +10,20 @@ public class LinkedListMain {
         addingElements();
         insertingData();
         pushData();
+        popData();
     }
 
-    public static void pushData() {
-        System.out.println("pushing the elements 56,30 and 70 ");
-        Stack stack=new Stack();
-        stack.push(56);
-        stack.push(30);
-        stack.push(70);
-        stack.DisplayStack();
-    }
 
     public static void createSimpleLinkedList() {
         LinkedList<Integer> list = new LinkedList<>();
         list.add(56);
         list.add(30);
         list.add(70);
+        System.out.println("\n\n===================UC1=====================\n");
         System.out.println("the simple linkedList created = " + list);
     }
     public static void addingElements() {
+        System.out.println("\n==============================UC2============================");
         System.out.println("\nwhile adding, the new elements will get added in front " +
                 "\nso required output ===> 56,30,70");
         LinkedListMethods linkedListMethods=new LinkedListMethods();
@@ -39,6 +34,7 @@ public class LinkedListMain {
     }
 
     public static void appendingTheElements() { // new element gets added at the end
+        System.out.println("\n==============================UC3============================");
         LinkedListMethods linkedListMethods=new LinkedListMethods();
         System.out.println("while appending, the new elements will get added to the end position" +
                 "\nso required output is ====> 56,30,70");
@@ -46,6 +42,7 @@ public class LinkedListMain {
         linkedListMethods.append(30);
         linkedListMethods.append(70);
         linkedListMethods.display();
+
     }
 
     public static void insertingData() {
@@ -53,7 +50,33 @@ public class LinkedListMain {
         linkedListMethods.add(56);
         linkedListMethods.append(70);
         linkedListMethods.insert(1,30);
+        System.out.println("\n=================UC4==================");
         System.out.println("inserting 30 between 56 and 70");
         linkedListMethods.display();
+        System.out.println("=========================================\n");
+
     }
+    public static void pushData() {
+        System.out.println("pushing the elements 56,30 and 70 ");
+        Stack stack=new Stack();
+        stack.push(56);
+        stack.push(30);
+        stack.push(70);
+        stack.DisplayStack();
+    }
+
+    public static void popData() {//input 56,30,70
+        Stack stack=new Stack();
+        stack.push(56);
+        stack.push(30);
+        stack.push(70);
+        System.out.println("data is : ");
+        stack.DisplayStack();
+        System.out.println("==================UC5==================");
+        System.out.println("required output sequence (after deleting first element ) ==> 30,70");
+        System.out.println("Pop value is : "+stack.pop());
+        System.out.println("=============================================================\n");
+    }
+
+
 }
--- src/com/java/Stack.java
+++ src/com/java/Stack.java
@@ -4,15 +4,22 @@ import org.w3c.dom.ls.LSOutput;
 
 public class Stack {
     int stackArray[]=new int[3];
-    int count=0;
+    int pushCount=0,popCount=0;
     public void push(int data){
-        stackArray[count]=data;
-        count++;
+        stackArray[pushCount]=data;
+        pushCount++;
     }
     public void DisplayStack(){
         for(int n: stackArray){
             System.out.println(n+" ");
         }
+    }
+    public int pop(){
+        int data;
+        data = stackArray[popCount];
+        stackArray[popCount]=0;
+        DisplayStack();
+        return data;
     }
 
 }
